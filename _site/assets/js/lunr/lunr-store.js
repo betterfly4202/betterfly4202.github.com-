6fa24d4e54a68d419cbd7169b7bb9d5df6ee97db
이충일 <betterfly4202@gmail.com>
2018-11-12 02:25:57 +0900

    드디어 notes 분리

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,16 @@
 var store = [{
-        "title": "새단장",
-        "excerpt":"블로그 새단장  ","categories": ["Edge Case"],
+        "title": "category : db",
+        "excerpt":"db  ","categories": ["db"],
         "tags": ["content"],
-        "url": "http://localhost:4000/edge%20case/1/",
+        "url": "http://localhost:4000/db/2/",
+        "teaser":null},{
+        "title": "category : JAVA",
+        "excerpt":"JAVA  ","categories": ["java"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/java/1/",
+        "teaser":null},{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/1/",
         "teaser":null}]
