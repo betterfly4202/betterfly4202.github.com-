27d8dfafffc6c777c64d86d51e6747402ad86de2
이충일 <betterfly4202@gmail.com>
2018-11-11 18:39:23 +0900

    aaa

---
+++ _docs/01-start_dev.md
@@ -0,0 +1,11 @@
+---
+title: "dddeevvv"
+permalink: /dev/java/
+excerpt: "java stroy"
+last_modified_at: 2018-10-04T20:15:56-04:00
+redirect_from:
+  - /theme-setup/
+toc: true
+---
+
+# DEV START
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that      Lists within lists do not break the ordered list numbering order   Your list styles go deep enough.   Ordered – Unordered – Ordered      ordered item   ordered item            unordered       unordered                 ordered item       ordered item           ordered item   ordered item   Ordered – Unordered – Unordered      ordered item   ordered item            unordered       unordered                 unordered item       unordered item           ordered item   ordered item   Unordered – Ordered – Unordered      unordered item   unordered item            ordered       ordered                    unordered item           unordered item                           unordered item   unordered item   Unordered – Unordered – Ordered      unordered item   unordered item            unordered       unordered                    ordered item           ordered item                           unordered item   unordered item  ","categories": ["Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "http://localhost:4000/edge%20case/edge-case-nested-and-mixed-lists/",
+        "title": "새단장",
+        "excerpt":"블로그 새단장  ","categories": ["Edge Case"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/edge%20case/1/",
         "teaser":null}]
