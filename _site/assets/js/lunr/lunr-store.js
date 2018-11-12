0dcbe460a9b1cc47ed9f9e886461542c476b926c
이충일 <betterfly4202@gmail.com>
2018-11-12 23:14:45 +0900

    blog making

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,33 @@
 var store = [{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/3/",
+        "teaser":null},{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/1/",
+        "teaser":null},{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/2/",
+        "teaser":null},{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/2/",
+        "teaser":null},{
         "title": "category : db",
-        "excerpt":"zz   내용을 입력합니다  ","categories": ["DB"],
+        "excerpt":"https://mmistakes.github.io/minimal-mistakes/collection-archive/   내용을 입력합니다  ","categories": ["DB"],
         "tags": ["content"],
         "url": "http://localhost:4000/db/2/",
+        "teaser":null},{
+        "title": "새단장",
+        "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
+        "tags": ["content"],
+        "url": "http://localhost:4000/notes/2/",
         "teaser":null},{
         "title": "category : JAVA",
         "excerpt":"JAVA  ","categories": ["JAVA"],
@@ -12,5 +37,5 @@ var store = [{
         "title": "새단장",
         "excerpt":"블로그 새단장   블로그 내용을 씁니다.  ","categories": ["notes"],
         "tags": ["content"],
-        "url": "http://localhost:4000/notes/1/",
+        "url": "http://localhost:4000/notes/3/",
         "teaser":null}]
